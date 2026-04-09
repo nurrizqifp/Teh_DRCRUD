83cf75b74d48a81f42bced8becf333c86730d603
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-04-09 15:18:07 +0700

    Wired the dataGridView1_CellClick method to the CellClick event of dataGridView1. This enables handling user interactions when a cell in the DataGridView is clicked.

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -179,6 +179,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(705, 135);
             this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // btnCreate
             // 
